8bc4e7b3d6b7b509947fa93c37a35681aa5054bb
Alessandro Iannacone Romero <alex1400ahjulio@gmail.com>
2025-09-27 22:56:50 -0500

    Commit- Pedido
    Aqui hemos creado una clase pedido, hemos declarados sus variables como combo,bebida, acompañamiento y extra y hemos creado un mostrarResumen y lo que retornara.

--- EJERCICIO2_POO2_/src/model/Pedido.java
+++ EJERCICIO2_POO2_/src/model/Pedido.java
@@ -1,13 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package model;
 
-/**
- *
- * @author PC
- */
+
 public class Pedido {
+    private Combo combo;
+    private String bebida;
+    private String acompañamiento;
+    private String extra;
     
+    public Pedido(Combo combo, String bebida, String acompañamiento, String extra){
+        this.combo = combo;
+        this.bebida = bebida;
+        this.acompañamiento = acompañamiento;
+        this.extra = extra;
+    }
+    public String mostrarResumen(){
+        return "===  Resumen de pedido ===\n"+ 
+                combo.mostrarDetalles() + "\n"+
+                "Bebida: " + bebida + "\n"+
+                "Acompañamiento: " + acompañamiento + "\n" +
+                "Extra: " + extra + "\n";
+                        
+    }
 }
