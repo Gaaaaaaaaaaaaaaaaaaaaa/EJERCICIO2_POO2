3c6bb96ccda9fc560477ea6c9d9bbe4acbfa8712
Alessandro Iannacone Romero <alex1400ahjulio@gmail.com>
2025-09-27 23:08:34 -0500

    Commit- ComboBuilder
    Aqui hemos declarado el combobuilder, donde declaramos todas las variables y usamos los setters para cada uno y declaramos otra clase pedido build que retoranara una nueva instancia pedido con dichos parametros.

--- EJERCICIO2_POO2_/src/model/ComboBuilder.java
+++ EJERCICIO2_POO2_/src/model/ComboBuilder.java
@@ -1,13 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package model;
 
-/**
- *
- * @author PC
- */
+
 public class ComboBuilder {
+    private Combo combo;
+    private String bebida;
+    private String acompañamiento;
+    private String extra;
     
+    public ComboBuilder setCombo(Combo combo){
+        this.combo = combo;
+        return this;
+    }
+    public ComboBuilder setBebida(String bebida){
+        this.bebida = bebida;
+        return this;
+    }
+    public ComboBuilder setAcompañamiento(String acompañamiento){
+        this.acompañamiento = acompañamiento;
+        return this;
+    }
+    public ComboBuilder setExtra (String extra){
+        this.extra = extra;
+        return this;
+    }
+    
+      public Pedido build(){
+          return new Pedido(combo,bebida, acompañamiento, extra);
+      }
 }
