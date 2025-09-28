7af3b5318636fd65c5df568f5ceebfa69e5e5ddf
Alessandro Iannacone Romero <alex1400ahjulio@gmail.com>
2025-09-28 01:27:21 -0500

    Cambio de main
    añadi el runnable como c y añadi c.run();

--- EJERCICIO2_POO2_/src/Main/Main.java
+++ EJERCICIO2_POO2_/src/Main/Main.java
@@ -2,11 +2,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package view;
+package Main;
 import View.HomeView;
+
 public class Main {
     public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(() -> new HomeView());
+        Runnable c = () -> new HomeView();
+    c.run();
+
     }
 }
 
